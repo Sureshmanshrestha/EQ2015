10281f7987af5c662a566b14b6951a02c60f385b
Sureshmanshrestha <sureshmanshrestha.gh@gmail.com>
2025-09-03 12:33:21 +0545

    Add files via upload

---
+++ capitalhq.js
@@ -0,0 +1,10 @@
+var capitalhq = [{
+
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "STATUS": "Capital", "HQNAME": "Kathmandu", "RADIUS": 8 }, "geometry": { "type": "Point", "coordinates": [ 85.3247, 27.6973 ] } }
+]
+}
+];
